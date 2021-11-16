aa079692219bc8e3990e09d58af2a9b1d3fec7ab
Jose Chourio <jchouriopirela@gmail.com>
2021-11-16 00:18:00 -0400

    Added views for create, update, delete and list products. Added endpoints for consume that views

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
-    path('/products'), # List all products
-    path('/products/<str:pk>'), # Details
-    path('/shops/<str:shop_pk>/products'), # List and Create new products
-    path('/shops/<str:shop_pk>/products/<str:pk>'), # Update and delete products 
+    path('products', ProductsAPIView.as_view(), name='products_apiview'), # List all products
+    path('products/<str:pk>', ProductAPIView.as_view(), name='product_apiview'), # Details
+    path('shops/<str:shop_pk>/products', ShopProductsAPIView.as_view(), name='shop_products_apiview'), # List and Create new products
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from django.contrib.gis.measure import Distance
+from django.contrib.gis.geos import Point
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Product
+from .serializers import ListProductSerializer, CreateProductSerializer
+from .permissions import IsProductOwner
+from apps.shops.models import Shop
+
+
+class ProductsAPIView(APIView):
+
+    serializer = ListProductSerializer
+    permission_classes = (IsProductOwner, )
+
+    def get(self, request):
+        data = request.data
+        user = request.user
+
+        if 'location' in data or user.location:
+            if 'location' in data:
+                point = Point(*data.pop('location'))
+            else:
+                point = user.location
+            shops = Shop.objects.filter(
+                location__distance_lt=(
+                    point,
+                    Distance(m=5000)
+                )
+            )
+            products = Product.objects.filter(shop__in=shops, **data)
+        else:
+            products = Product.objects.filter(**data)
+        
+        serializer = self.serializer(products, many=True)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+    
+
+class ShopProductsAPIView(APIView):
+    serializer_classes = {
+        'list': ListProductSerializer,
+        'create': CreateProductSerializer,
+    }
+    default_serializer_class = ListProductSerializer
+    permission_classes = (IsProductOwner, )
+
+    def get_serializer_class(self, action):
+        return self.serializer_classes.get(action, self.default_serializer_class)
+    
+    def post(self, request, shop_pk):
+        data = request.data
+        shop = get_object_or_404(Shop, pk=shop_pk)
+        serializer = self.get_serializer_class('create')(data=data)
+
+        if serializer.is_valid():
+            product = serializer.save(shop=shop)
+            serializer = self.get_serializer_class('list')(instance=product)
+            return Response(
+                {'data': serializer.data},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+    
+    def get(self, request, shop_pk):
+        data = request.data
+        shop = get_object_or_404(Shop, pk=shop_pk)
+        products = Product.objects.filter(shop=shop, **data)
+        serializer = self.get_serializer_class('list')(products, many=True)
+
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
+
+class ProductAPIView(APIView):
+    serializer_classes = {
+        'list': ListProductSerializer,
+        'create': CreateProductSerializer
+    }
+    default_serializer_class = ListProductSerializer
+    permission_class = (IsProductOwner, )
+
+    def get_serializer_class(self, action):
+        return self.serializer_classes.get(action, self.default_serializer_class)
+
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = self.get_serializer_class('list')(instance=product)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        self.check_object_permissions(request, product)
+        serializer = self.get_serializer_class('create')(product, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'data': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        self.check_object_permissions(request, product)
+        product.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
