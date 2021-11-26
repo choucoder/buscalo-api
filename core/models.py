21138ba75f1699af50d2e7501f3641219daa9e98
Jose Chourio <jchouriopirela@gmail.com>
2021-11-25 23:49:25 -0400

    Added model and serializer for Address model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(
+        max_length=32
+    )
+    country_code = models.CharField(
+        max_length=16, blank=True, null=True
+    )
+    state = models.CharField(max_length=64, null=True, blank=True)
+    city = models.CharField(max_length=64, null=True, blank=True)
+    address = models.CharField(max_length=1024, null=True, blank=True)
+
+
+    def __str__(self):
+        return "{country}, {state}, {city}, {address}".format(
+            country=self.country,
+            state=self.state,
+            city=self.city,
+            address=self.address
+        )
