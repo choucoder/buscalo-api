15ebf8d1fa1abfdaeb7542603885d1a6f6be7340
Jose Chourio <jchouriopirela@gmail.com>
2022-03-17 23:36:35 -0400

    Added user serializer to feed serializer

--- apps/feeds/serializers.py
+++ apps/feeds/serializers.py
@@ -2,11 +2,12 @@ from rest_framework_gis.serializers import GeoModelSerializer
 
 from .models import Feed
 from apps.posts.serializers import ListPostSerializer
-
+from users.serializers import UserSerializer
 
 class FeedSerializer(GeoModelSerializer):
     post = ListPostSerializer(many=False, read_only=True)
-
+    user = UserSerializer(many=False, read_only=True)
+    
     class Meta:
         model = Feed
-        exclude = ['user']
+        fields = ('__all__')
